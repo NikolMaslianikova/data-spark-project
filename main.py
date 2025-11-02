205d13f1a9f9eaceb0321bf771bc37d2811dc868
Nikol Maslianikova <nikol.maslianikova.shi.2022@lpnu.ua>
2025-11-02 10:41:35 +0200

    Initial commit: схема DataFrame Spark-проєкту

---
+++ main.py
@@ -0,0 +1,37 @@
+from pyspark.sql import SparkSession
+from src.io_utils import load_weather_data
+import pandas as pd
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("WeatherInDocker").getOrCreate()
+
+    print("\n=== ЕТАП ВИДОБУВАННЯ ДАНИХ ===\n")
+
+    df = load_weather_data(spark, "/app/data/*.csv")
+
+    print("ЗАГАЛЬНА ІНФОРМАЦІЯ")
+    print(f"Кількість рядків: {df.count():,}")
+    print(f"Кількість колонок: {len(df.columns)}\n")
+
+    print("СПИСОК УСІХ КОЛОНОК:")
+    for i, c in enumerate(df.columns, start=1):
+        print(f"{i:>2}. {c}")
+    print()
+
+    print("СХЕМА DataFrame:")
+    df.printSchema()
+
+    print("\nПЕРШІ 5 РЯДКІВ ДАНИХ:\n")
+    df_safe = df.withColumn("date", df["date"].cast("string"))
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.width", None)
+    pd.set_option("display.precision", 3)
+    pandas_df = df_safe.limit(5).toPandas()
+
+    print("-" * 120)
+    print(pandas_df.to_string(index=False))
+    print("-" * 120)
+
+    print("\nЗавантаження даних успішно завершено.\n")
+
+    spark.stop()
